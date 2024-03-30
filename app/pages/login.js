7fd23c1c72e7b098e21602ad4381eafc5efc16e7
Chinmoy Biswas <chinmoy709@gmail.com>
2024-03-31 03:25:34 +0600

    initialized project

---
+++ app/pages/login.js
@@ -0,0 +1,12 @@
+import styles from "./styles/login.module.css";
+
+const LoginPage = () => {
+  return (
+    <div className={styles.loginContainer}>
+      <section className={styles.firstColumn}>col-1</section>
+      <section className={styles.secondColumn}>col-2</section>
+    </div>
+  );
+};
+
+export default LoginPage;
